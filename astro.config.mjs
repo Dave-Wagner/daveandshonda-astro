9fd1bbdb314aca38c72ca7c9e1b64a77e4db09ef
David Wagner <20711387+Dave-Wagner@users.noreply.github.com>
2024-08-23 11:54:12 -0400

    chore: Add tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
 import deno from "@deno/astro-adapter";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: deno({
     // Options
-    
   }),
+  integrations: [tailwind()]
 });
