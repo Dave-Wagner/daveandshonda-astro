943e6de37d0322728c7fc7846aaf01da4ad0238a
David Wagner <20711387+Dave-Wagner@users.noreply.github.com>
2024-08-23 12:12:31 -0400

    chore: Update Astro configuration for React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 import deno from "@deno/astro-adapter";
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: deno({
     // Options
   }),
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()]
 });
