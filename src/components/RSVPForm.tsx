e814826f0115d6e3f729f5621e4d406267c9b5ac
David Wagner <20711387+Dave-Wagner@users.noreply.github.com>
2024-08-27 15:55:26 -0400

    refactor: Add RSVPForm component to RSVP page and handle form submission

---
+++ src/components/RSVPForm.tsx
@@ -0,0 +1,236 @@
+import React, { useState } from 'react';
+import type { ChangeEvent, FormEvent } from 'react';
+
+interface FormData {
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  food_allergies: string;
+  special_considerations: string;
+  guest_count: number;
+  guest_count_child: number;
+  message: string;
+}
+
+export default function RSVPForm() {
+    const [canAttend, setCanAttend] = useState<boolean>(true);
+    const [formData, setFormData] = useState<FormData>({
+        first_name: '',
+        last_name: '',
+        email: '',
+        phone: '',
+        food_allergies: 'None',
+        special_considerations: 'None',
+        guest_count: 1,
+        guest_count_child: 0,
+        message: '',
+    });
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setFormData(prevData => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        // Custom validation logic
+        if (!formData.first_name || !formData.last_name || !formData.email) {
+            alert("Please provide your first name, last name, and email.");
+            return;
+        }
+
+        if (canAttend && (!formData.phone || formData.guest_count < 1)) {
+            alert("Please provide all the required fields if you are attending.");
+            return;
+        }
+
+        const formDataToSend = new FormData();
+        Object.entries(formData).forEach(([key, value]) => {
+            if (canAttend || ['first_name', 'last_name', 'email'].includes(key)) {
+                formDataToSend.append(key, value.toString());
+            }
+        });
+        formDataToSend.append('can_attend', canAttend.toString());
+
+        try {
+            const response = await fetch('/api/rsvp', {
+                method: 'POST',
+                body: formDataToSend,
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                alert(data.message); // Success message
+            } else {
+                alert(data.message); // Error message
+            }
+        } catch (error) {
+            alert('An error occurred while submitting the form. Please try again.');
+        }
+    }
+
+    return (
+        <form className="p-2" onSubmit={handleSubmit} noValidate>
+            <label className="input input-bordered flex items-center gap-2 m-2">
+                First Name:
+                <input 
+                    type="text" 
+                    className="grow" 
+                    placeholder="Daisy" 
+                    name="first_name" 
+                    value={formData.first_name}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <label className="input input-bordered flex items-center gap-2 m-2">
+                Last Name:
+                <input 
+                    type="text" 
+                    className="grow" 
+                    placeholder="Duck" 
+                    name="last_name" 
+                    value={formData.last_name}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <label className="input input-bordered flex items-center gap-2 m-2">
+                Email:
+                <input 
+                    type="email" 
+                    className="grow" 
+                    placeholder="daisy@duck.com" 
+                    name="email" 
+                    value={formData.email}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <div className="form-control">
+                <label className="label cursor-pointer">
+                    <span className="label-text">Yes, I'll be there!</span>
+                    <input
+                        type="radio"
+                        name="can_attend"
+                        className="radio checked:bg-success"
+                        checked={canAttend}
+                        onChange={() => setCanAttend(true)}
+                    />
+                </label>
+            </div>
+            <div className="form-control">
+                <label className="label cursor-pointer">
+                    <span className="label-text">Sorry, I won't be able to attend.</span>
+                    <input
+                        type="radio"
+                        name="can_attend"
+                        className="radio checked:bg-error"
+                        checked={!canAttend}
+                        onChange={() => setCanAttend(false)}
+                    />
+                </label>
+            </div>
+
+            {canAttend && (
+                <>
+                    <label className="input input-bordered flex items-center gap-2 m-2">
+                        Phone:
+                        <input 
+                            type="tel" 
+                            className="grow" 
+                            placeholder="555-555-5555" 
+                            name="phone" 
+                            value={formData.phone}
+                            onChange={handleInputChange}
+                        />
+                    </label>
+
+                    <div className="form-control">
+                        <label className="label cursor-pointer">
+                            <span className="label-text">Food Allergies?</span>
+                            <span className="label-text-alt">Please list any food allergies you have.</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="None"
+                            className="input input-bordered w-full grow"
+                            name="food_allergies"
+                            value={formData.food_allergies}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+
+                    <div className="form-control">
+                        <label className="label cursor-pointer">
+                            <span className="label-text">Special Considerations?</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="None"
+                            className="input input-bordered w-full grow"
+                            name="special_considerations"
+                            value={formData.special_considerations}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+
+                    <label className="form-control w-full max-w-xs m-2">
+                        <div className="label">
+                            <span className="label-text">Number of Adult Guests?</span>
+                            <span className="label-text-alt">Including yourself</span>
+                        </div>
+                        <input
+                            type="number"
+                            placeholder="1"
+                            className="input input-bordered w-full max-w-xs grow"
+                            name="guest_count"
+                            value={formData.guest_count}
+                            onChange={handleInputChange}
+                        />
+                    </label>
+
+                    <label className="form-control w-full max-w-xs m-2">
+                        <div className="label">
+                            <span className="label-text">Number of Child Guests?</span>
+                        </div>
+                        <input
+                            type="number"
+                            placeholder="0"
+                            className="input input-bordered w-full max-w-xs grow"
+                            name="guest_count_child"
+                            value={formData.guest_count_child}
+                            onChange={handleInputChange}
+                        />
+                    </label>
+
+                    <label className="textarea textarea-bordered flex items-center gap-2 m-2">
+                        Message:
+                        <textarea
+                            className="textarea"
+                            placeholder="I can't wait to see you!"
+                            name="message"
+                            value={formData.message}
+                            onChange={handleInputChange}
+                        />
+                    </label>
+                </>
+            )}
+
+            <div className="flex gap-4 justify-center m-6">
+                <button type="submit" className="btn btn-primary">
+                    Send RSVP
+                    <i className="fa-solid fa-arrow-right text-sm"></i>
+                </button>
+
+                <a className="btn btn-neutral">
+                    See our blog
+                    <i className="fa-solid fa-blog"></i>
+                </a>
+            </div>
+        </form>
+    );
+}
--- src/pages/api/rsvp.ts
+++ src/pages/api/rsvp.ts
@@ -5,23 +5,61 @@ export const POST: APIRoute = async ({ request }) => {
     const first_name = data.get("first_name");
     const last_name = data.get("last_name");
     const email = data.get("email");
+    const canAttend = data.get("can_attend") === "true"; // Convert to boolean
     const phone = data.get("phone");
-    const guest_count = data.get("guest_count");
+    const guest_count_adult = data.get("guest_count");
+    const guest_count_child = data.get("guest_count_child");
+    const food_allergies = data.get("food_allergies");
+    const special_considerations = data.get("special_considerations");
     const message = data.get("message");
 
-    // Validate the data - TODO: you'll probably want to do more than this
-    if (!first_name || !last_name || !email || !phone || !guest_count) {
+    // Basic validation for required fields
+    if (!first_name || !last_name || !email) {
         return new Response(
             JSON.stringify({
-                message: "Missing required fields",
+                message: "Please provide your first name, last name, and email."
             }),
             { status: 400 }
         );
     }
-    // Do something with the data, then return a success response
+
+    // If the user is not attending, no need to validate further
+    if (!canAttend) {
+        return new Response(
+            JSON.stringify({
+                message: "Thank you for letting us know. Your RSVP has been recorded."
+            }),
+            { status: 200 }
+        );
+    }
+
+    // If the user is attending, validate only the fields that are required
+    if (canAttend) {
+        if (!phone) {
+            return new Response(
+                JSON.stringify({
+                    message: "Please provide a contact phone number."
+                }),
+                { status: 400 }
+            );
+        }
+
+        // Convert guest_count_adult to a number and check if it's valid
+        const guestCountAdult = guest_count_adult ? parseInt(guest_count_adult.toString(), 10) : 0;
+        if (guestCountAdult < 1) {
+            return new Response(
+                JSON.stringify({
+                    message: "Please provide the number of adult guests attending."
+                }),
+                { status: 400 }
+            );
+        }
+    }
+
+    // Further processing of the data if necessary...
     return new Response(
         JSON.stringify({
-            message: "Success!"
+            message: "Thank you for your RSVP! We look forward to seeing you."
         }),
         { status: 200 }
     );
